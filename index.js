d55b9dcdddda0c3e9b8add7f186ad4bf67df3eb5
Nishant Kumar Singh <nishant220902@gmail.com>
2025-04-11 18:45:30 +0530

    update the comment of rate limiter

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 200, // limit each IP to 100 requests per windowMs
+  max: 200, // limit each IP to 200 requests per windowMs
 });
 
 // Middleware
