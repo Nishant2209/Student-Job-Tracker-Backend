8407d2a21bf8fbd57cc14a31ccf2fac29389218f
Nishant Kumar Singh <nishant220902@gmail.com>
2025-04-11 18:52:07 +0530

    update the localhost url for the cors

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const limiter = rateLimit({
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
